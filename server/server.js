bf924629c09833c249ca73a5c87d244c9d76948a
Miguel Salinas Gancedo <miguel@thingtrack.com>
2018-09-27 13:23:53 +0200

    last update

--- server/server.js
+++ server/server.js
@@ -10,7 +10,8 @@ app.start = function() {
   return app.listen(function() {
     app.emit('started');
     var baseUrl = app.get('url').replace(/\/$/, '');
-    console.log('Web server listening at: %s', baseUrl);
+    console.log('Web server listening at: %s', baseUrl);        
+    //if (app.get('loopback-component-explorer') && process.env.NODE_ENV == 'development') {
     if (app.get('loopback-component-explorer')) {
       var explorerPath = app.get('loopback-component-explorer').mountPath;
       console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
